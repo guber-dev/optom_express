71ca0a2d6f49680dcccc0dbb6013b2dd6c2ba04a
Mirlan Akimov <mirlanakimov@Mirlans-MacBook-Pro.local>
2025-07-01 15:33:48 +0600

    Добавлены новые продукты (овощи и фрукты)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import styled, { createGlobalStyle } from 'styled-components';
 import { Header } from './components/Header';
 import { PriceList } from './components/PriceList';
